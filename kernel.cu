6b230bbbe88f6d43de092ed7db1026d2fe165b21
judyyuyjs <165414066+judyyuyjs@users.noreply.github.com>
2025-04-15 07:31:38 -0400

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -1,98 +1,69 @@
-__global__ void vecDiff(const float* A, const float* B, float* C, int n) {
+__global__ void vecDiff_cN_bar(const int* v_index, const float* v, const float* w, float* r, int n) {
     int i = blockDim.x*blockIdx.x + threadIdx.x;
     if (i < n) {
-        C[i] = A[i] - B[i];
+        r[i] = v[v_index[i]] - w[i];
     }
 }
+
 __global__ void vecDiv(const float* A, const float* B, float* C, int n) {
     int i = blockDim.x*blockIdx.x + threadIdx.x;
     if (i < n) {
         C[i] = A[i] / B[i];
     }
 }
-__global__ void vecDot(const float* A, const float* B, float* C, int n) {
+
+__global__ void vecMultiConst(float* A, const float scalar, int n) {
     int i = blockDim.x*blockIdx.x + threadIdx.x;
     if (i < n) {
-        C[i] = A[i] * B[i];
+        A[i] = A[i] * scalar;
     }
 }
-
 // matrix: n*n
 // vec: n*1
 __global__ void matrixVecMulti(const float *matrix, const float *vec, float *c, int n) {
     int r = blockDim.x*blockIdx.x + threadIdx.x;
     if(r < n) {
-	float sum = 0.0f;
+        float sum = 0.0f;
         for (int i = 0; i < n; i++) {
             sum += matrix[r*n+i] * vec[i];
-	}
-	c[r] = sum;
+        }
+        c[r] = sum;
+    }
+}
+__global__ void vecMatrixMulti(const float *vec, const float *matrix, const int *matrix_index, float *r, int m, int n) {
+    int c = blockDim.x*blockIdx.x + threadIdx.x;
+    if(c < m) {
+        float sum = 0.0f;
+        for (int i = 0; i < n; i++) {
+            sum += vec[i] * matrix[matrix_index[i]*m+c];
+        }
+        r[c] = sum;
     }
 }
 // vec: 1*n
 // matrix: n*n
-__global__ void vecMatrixMulti(const float *vec, const float *matrix, float *r, int n) {
-    int c = blockDim.x*blockIdx.x + threadIdx.x;
-    if(c < n) {
-	float sum = 0.0f;
-	for (int i = 0; i < n; i++) {
-            sum += vec[i] * matrix[i*n+c];
-	}
-	r[c] = sum;
-    }
-}
 __global__ void vecMatrixMulti_cB(const int *orig_vec_index, const float *orig_vec, const float *matrix, float *r, int n) {
     int c = blockDim.x*blockIdx.x + threadIdx.x;
     if(c < n) {
-	float sum = 0.0f;
+        float sum = 0.0f;
         for (int i = 0; i < n; i++) {
-	    sum += orig_vec[orig_vec_index[i]] * matrix[i*n+c];
-	}
-	r[c] = sum;
+            sum += orig_vec[orig_vec_index[i]] * matrix[i*n+c];
+        }
+        r[c] = sum;
     }
 }
-/*
-__iglobal__ void updateE(float *E, const float A_bar, int leaving index, int n) {
-   int i = blockDim.x*blockIdx.x + threadIdx.x;
-   E[i] = 0.0f;
-   if(i < n) {
-       
+
+__global__ void B_inverse_col(float *col, const float *B_inverse, int col_index, int n) {
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if (i < n) {
+        col[i] = B_inverse[col_index*n+i];
+    }
+}
+
+__global__ void updateB_inverse(float *B_inverse, const float *eta_col, const float *row_r_copy, int n) {
+   int row = blockIdx.x * blockDim.x + threadIdx.x;
+   int col = blockIdx.y * blockDim.y + threadIdx.y;
+   if ((row < n) && (col < n)) {
+       B_inverse[row*n+col] += eta_col[row] * row_r_copy[col];
    }
 }
-*/
-__global__ void matrixMulti(const float *a, const float *b, float *c, int n) {
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-    int col = blockIdx.y * blockDim.y + threadIdx.y;
-    if ((row < n) && (col < n)) {
-	float sum = 0.0f;
-        for (int i = 0; i < n; i++) {
-            sum += a[row*n+i] * b[i*n+col];
-        }
-	c[row*n+col] = sum;
-    }
-}
-
-__global__ void SpMV_ELL(int num_rows, float *data, unsigned int *col_index, int num_elem, float *x, float *y) {
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row < num_rows) {
-	float dot = 0;
-	for (int i = 0; i < num_elem; i++) {
-            dot += data[row+i*num_rows]*x[col_index[row+i*num_rows]];
-        }
-        y[row] = dot;
-    }
-}
-
-__global__ void SpMV_CSR(int num_rows, float *data, int *col_index, int *row_ptr, float *x, float *y) {
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row < num_rows) {
-	float dot = 0;
-	int row_start = row_ptr[row];
-	int row_end = row_ptr[row+1];
-	for (int elem = row_start; elem < row_end; elem++) {
-            dot += data[elem] * x[col_index[elem]];
-	}
-	y[row] = dot;
-    }
-}
-
